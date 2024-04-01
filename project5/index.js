b30e1234b4ed79375483d14da9e70f4881b35ba2
nawaisjan <98877121+nawaisjan@users.noreply.github.com>
2024-04-02 04:53:46 +0500

    second commit

---
+++ project5/index.js
@@ -0,0 +1,25 @@
+const randomColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+let intervalId;
+
+const startChangingColor = function () {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = randomColor();
+  }, 1000);
+};
+
+const stopCahnagingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+console.log(randomColor());
+document.querySelector("#start").addEventListener("click", startChangingColor);
+document.querySelector("#stop").addEventListener("click", stopCahnagingColor);
